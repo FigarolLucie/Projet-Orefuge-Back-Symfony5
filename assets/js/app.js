2f154c1d06021cfd58d56c1d49fd9c66eb1d8fb6
FigarolLucie <lucie.figarol.pro@gmail.com>
2020-05-21 11:07:52 +0200

    Menu desktop + mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,31 @@
 import '../css/app.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
+import $ from 'jquery';
+var app = {
 
+  init: function () {
+    console.log('init');
+
+    document.getElementById('icon-menu').addEventListener('click', app.handleDisplayMenu);
+    document.getElementById('icon-cross').addEventListener('click', app.handleDisplayMenu);
+
+    var widthWindow = document.body.clientWidth;
+
+    // if( widthWindow <= 800 )
+    // {
+      
+    // }
+
+  },
+
+  handleDisplayMenu: function (evt) {
+    evt.preventDefault();
+    const btnMenu = $('#nav__item__mobile');
+    btnMenu.toggle();
+
+  }
+
+};
+document.addEventListener('DOMContentLoaded', app.init);
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
